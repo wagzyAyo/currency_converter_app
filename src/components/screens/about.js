2bc3788ec2bf29da6fc5551bf1e70ad96353900b
Ayo <talktojmcvibes@gmail.com>
2024-01-21 16:19:56 +0100

    text about and privacy

--- src/components/screens/about.js
+++ src/components/screens/about.js
@@ -1,12 +1,28 @@
 import React from "react";
-import {Text, View, StyleSheet} from 'react-native'
+import {Text, View, StyleSheet, SafeAreaView, StatusBar} from 'react-native'
 
 const About = () => {
     return(
-        <View>
-            <Text>This is about Page</Text>
+        <SafeAreaView>
+        <View style={styles.container}>
+            <Text style={styles.textwrapper}>We believe that financial transactions should
+            be as borderless as the modern world we live 
+            in. Whether you're a frequent traveler</Text>
         </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 0,
+        marginTop: StatusBar.currentHeight || 0,
+        padding: 20,
+    },
+    textwrapper: {
+        textAlign: 'center'
+    }
+})
+
 
 export default About
--- src/components/screens/privacy.js
+++ src/components/screens/privacy.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
 
 const Privacy = () => {
     return(
-        <View>
-            <Text>This is the privacy route</Text>
+        <SafeAreaView >
+        <View style={styles.container}>
+            <Text style={styles.textwrapper}>This Privacy Policy is designed to help you understand
+            how we collect, use, disclose, and safeguard your personal 
+            information.</Text>
         </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0,
+        marginTop: StatusBar.currentHeight || 0,
+        padding: 20,
+    },
+    textwrapper: {
+        textAlign: 'center'
+    }
+})
 
 export default Privacy
