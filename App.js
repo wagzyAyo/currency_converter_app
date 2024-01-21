0d80135b5674211b7de5610163c4e27f197b5a57
Ayo <talktojmcvibes@gmail.com>
2024-01-21 15:39:41 +0100

    nav icons

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import About from './src/components/screens/about';
 import Privacy from './src/components/screens/privacy';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome, MaterialIcons } from '@expo/vector-icons'
 
 
 const Tabs = createBottomTabNavigator()
@@ -13,9 +14,21 @@ export default function App() {
   return (
     <NavigationContainer >
       <Tabs.Navigator>
-        <Tabs.Screen  name='Convert currency' component={Convert}/>
-        <Tabs.Screen name='About' component={About} />
-        <Tabs.Screen name='Privacy' component={Privacy} />
+        <Tabs.Screen  name='Convert currency' component={Convert} options={{
+          tabBarIcon: ({focused}) =>(
+            <FontAwesome name='exchange' size={22} color={ focused ? 'blue' : 'black' } />
+          )
+        }}/>
+        <Tabs.Screen name='About' component={About} options={{
+          tabBarIcon: ({ focused}) => (
+            <FontAwesome name='info-circle' size={22} color={focused ? 'blue' : 'black'} />
+          )
+        }} />
+        <Tabs.Screen name='Privacy' component={Privacy} options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialIcons name='privacy-tip' size={22} color={ focused ? 'blue' : 'black'} />
+          )
+        }} />
     </Tabs.Navigator>
     </NavigationContainer>
   );
