80e76fccc32975f911c319bea640dbe71ae70640
Ayo <talktojmcvibes@gmail.com>
2024-01-21 20:56:04 +0100

    image added

--- src/components/screens/about.js
+++ src/components/screens/about.js
@@ -1,10 +1,11 @@
 import React from "react";
-import {Text, View, StyleSheet, SafeAreaView, StatusBar} from 'react-native'
+import {Text, View, StyleSheet, SafeAreaView, StatusBar, Image} from 'react-native'
 
 const About = () => {
     return(
         <SafeAreaView>
         <View style={styles.container}>
+            <Image style={styles.img} source={require('../../../assets/unsplash_TamMbr4okv4.png')}/>
             <Text style={styles.textwrapper}>We believe that financial transactions should
             be as borderless as the modern world we live 
             in. Whether you're a frequent traveler</Text>
@@ -19,9 +20,15 @@ const styles = StyleSheet.create({
         marginTop: StatusBar.currentHeight || 0,
         padding: 20,
     },
+    img: {
+        width:370,
+        height:370,
+    },
     textwrapper: {
-        textAlign: 'center'
-    }
+        textAlign: 'center',
+        marginTop: 20,
+    },
+   
 })
 
 
--- src/components/screens/privacy.js
+++ src/components/screens/privacy.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, View, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, StatusBar, Image} from 'react-native';
 
 const Privacy = () => {
     return(
         <SafeAreaView >
         <View style={styles.container}>
+            <Image style={styles.img} source={require('../../../assets/unsplash_ah-HeguOe9k.png')}/>
             <Text style={styles.textwrapper}>This Privacy Policy is designed to help you understand
             how we collect, use, disclose, and safeguard your personal 
             information.</Text>
@@ -19,9 +20,15 @@ const styles = StyleSheet.create({
         marginTop: StatusBar.currentHeight || 0,
         padding: 20,
     },
+    img:{
+        width:370,
+        height:370,
+    },
     textwrapper: {
-        textAlign: 'center'
-    }
+        textAlign: 'center',
+        marginTop: 20,
+    },
+   
 })
 
 export default Privacy
